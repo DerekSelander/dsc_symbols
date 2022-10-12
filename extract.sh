27b1c677b2a043fdd13703a22db8dfb1afa61be8
LOLgrep <derekselander@yahoo.com>
2022-10-12 13:25:27 -0600

    cleaned up objc code

--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 
-ssh -n root@$1 'OUT_BASE="/tmp/dsc_symbols"; rm -rf $OUT_BASE; mkdir -p "$OUT_BASE"; /usr/local/bin/dsdump -l | grep -v \* | awk '"'"'{print $2}'"'"' | while read line; do echo  "processing \"$line\""; mkdir -p "$OUT_BASE/$(dirname $line)"; /usr/local/bin/dsdump "$line" -vUp | awk '"'"'{print "|| " $2 " " $3}'"'"' | sort > "$OUT_BASE/$line"; /usr/local/bin/dsdump "$line" -vvu | awk '"'"'{print "   " $2 " " $3}'"'"'  | sort >> "$OUT_BASE/$line"; if /usr/local/bin/dsdump "$line" -vU | grep OBJC_CLASS_; then /usr/local/bin/dsdump -o "$line" >> "$OUT_BASE/$line"; fi  ; done'
+ssh -n root@$1 'OUT_BASE="/tmp/dsc_symbols"; rm -rf $OUT_BASE; mkdir -p "$OUT_BASE"; /usr/local/bin/dsdump -l | grep -v \* | awk '"'"'{print $2}'"'"' | while read line; do echo  "processing \"$line\""; mkdir -p "$OUT_BASE/$(dirname $line)"; /usr/local/bin/dsdump "$line" -vUp | awk '"'"'{print "|| " $2 " " $3}'"'"' | sort > "$OUT_BASE/$line"; /usr/local/bin/dsdump "$line" -vu | awk '"'"'{print "__ " $2 " " $3}'"'"'  | sort >> "$OUT_BASE/$line"; if /usr/local/bin/dsdump "$line" -vU | grep -q OBJC_CLASS_; then /usr/local/bin/dsdump -o "$line" | sed -E '"'"'s/0x............//g'"'"' >> "$OUT_BASE/$line"; fi  ; done'
 
 
 echo "extracted some shit"
