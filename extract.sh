2b9811659b1720a9e942e1b86cb3595f5bfadcd5
LOLgrep <derekselander@yahoo.com>
2022-10-12 11:19:03 -0600

    Added objc and undefined symbols

--- extract.sh
+++ extract.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #set -x
 
-ssh -n root@$1 'OUT_BASE="/tmp/dsc_symbols"; mkdir -p "$OUT_BASE"; /usr/local/bin/dsdump -l | grep -v \* | awk '"'"'{print $2}'"'"' | while read line; do echo  "processing \"$line\""; mkdir -p "$OUT_BASE/$(dirname $line)"; /usr/local/bin/dsdump "$line" -vUp | awk '"'"'{print "|| " $2 " " $3}'"'"' | sort > "$OUT_BASE/$line"; /usr/local/bin/dsdump "$line" -vUP | awk '"'"'{print "   " $2 " " $3}'"'"' | grep -v "\<redacted\>" | sort >> "$OUT_BASE/$line" ; done'
+ssh -n root@$1 'OUT_BASE="/tmp/dsc_symbols"; rm -rf $OUT_BASE; mkdir -p "$OUT_BASE"; /usr/local/bin/dsdump -l | grep -v \* | awk '"'"'{print $2}'"'"' | while read line; do echo  "processing \"$line\""; mkdir -p "$OUT_BASE/$(dirname $line)"; /usr/local/bin/dsdump "$line" -vUp | awk '"'"'{print "|| " $2 " " $3}'"'"' | sort > "$OUT_BASE/$line"; /usr/local/bin/dsdump "$line" -vvu | awk '"'"'{print "   " $2 " " $3}'"'"'  | sort >> "$OUT_BASE/$line"; if /usr/local/bin/dsdump "$line" -vU | grep OBJC_CLASS_; then /usr/local/bin/dsdump -o "$line" >> "$OUT_BASE/$line"; fi  ; done'
 
 
 echo "extracted some shit"
 ssh root@$1 "tar -zcf /tmp/dsc_symbols.zip /tmp/dsc_symbols"
 scp "root@$1:/tmp/dsc_symbols.zip" "/tmp/dsc_symbols.zip"
 
+open -R "/tmp/dsc_symbols.zip"
