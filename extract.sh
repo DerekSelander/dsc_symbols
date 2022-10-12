e0b0cd88bb908572098918bf84f86beda8f27535
LOLgrep <derekselander@yahoo.com>
2022-10-12 01:36:33 -0600

    Better extraction script

--- extract.sh
+++ extract.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #set -x
 
-IMAGES=$(ssh root@$1 "/usr/local/bin/dsdump -l" | grep -v \* | awk '{print $2}' )
-#echo -e $IMAGES |  head -1 | while read IMAGE; do echo "dumping $IMAGE";  ssh root@$1 "/usr/local/bin/dsdump -v $IMAGE -U -p" | awk  '{print "|| " $2 "\ " $3}' | sort ; done
-while IFS= read -r line ; do echo "processing \"$line\""; mkdir -p "./$(dirname $line)";  ssh root@$1 "/usr/local/bin/dsdump $line -v -U -p" < /dev/null  |  awk  '{print "|| " $2 "\ " $3}' | sort  > ".$line" ;  ssh root@$1 "/usr/local/bin/dsdump $line -v -U -P" < /dev/null  |  awk  '{print "   " $2 "\ " $3}' | grep -v "\<redacted\>" | sort  >> ".$line";   done <<< "${IMAGES}"
-#while IFS= read -r line ; do mkdir -p "./$(dirname $line)"; ssh root@$1 "/usr/local/bin/dsdump $line -v -U -p" |  awk  '{print "|| " $2 "\ " $3}' >  ".$line";   done <<< "${IMAGES}"
-#while IFS= read -r line ; do mkdir -p "./$(dirname $line)"; ssh root@$1 "/usr/local/bin/dsdump $line -v -U -p" |  awk  '{print "|| " $2 "\ " $3}' >  ".$line";   done <<< "${IMAGES}"
-#echo -e $IMAGES |  head -1 | while read IMAGE; do echo "dumping $IMAGE";  ssh root@$1 "echo $IMAGE"; done
+ssh -n root@$1 'OUT_BASE="/tmp/dsc_symbols"; mkdir -p "$OUT_BASE"; /usr/local/bin/dsdump -l | grep -v \* | awk '"'"'{print $2}'"'"' | while read line; do echo  "processing \"$line\""; mkdir -p "$OUT_BASE/$(dirname $line)"; /usr/local/bin/dsdump "$line" -vUp | awk '"'"'{print "|| " $2 " " $3}'"'"' | sort > "$OUT_BASE/$line"; /usr/local/bin/dsdump "$line" -vUP | awk '"'"'{print "   " $2 " " $3}'"'"' | grep -v "\<redacted\>" | sort >> "$OUT_BASE/$line" ; done'
+
+
+echo "extracted some shit"
+ssh root@$1 "tar -zcf /tmp/dsc_symbols.zip /tmp/dsc_symbols"
+scp "root@$1:/tmp/dsc_symbols.zip" "/tmp/dsc_symbols.zip"
+
